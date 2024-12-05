9c390242bd8bfdf8e2f3ceeaa7cec2aae9bae40e
Philip Nations <167041497+pnations@users.noreply.github.com>
2024-12-05 10:21:38 -0600

    updated files

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  "mode": 'development',
+  "output": {
+    "filename": "[name].pack.js",
+    "path": path.resolve(__dirname),
+  },
+  "module": {
+    "rules": [
+      {
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "@babel/preset-env",
+              "@babel/preset-react",
+            ]
+          }
+        },
+        "exclude": /node_modules/,
+        "test": /\.js$/
+      }
+    ]
+  },
+  "entry": {
+    "index": "./index"
+  }
+};
